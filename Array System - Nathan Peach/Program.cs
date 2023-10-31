705e2fc8aa9c5634ba623866efd6131ea5a616a5
PeachCube <nathanpeach500@live.com>
2023-10-31 14:14:27 -0300

    added fire, showhud, and reload methods

--- Array System - Nathan Peach/Program.cs
+++ Array System - Nathan Peach/Program.cs
@@ -8,15 +8,30 @@ namespace Array_System___Nathan_Peach
 {
     internal class Program
     {
-        string[] weapons = ();
+        static int equipped;
+        static string[] weapons = {"Pistol", "Assault Rifle", "Shotgun", "Rocket Launcher" };
+        static int[] ammo = { 0, 0, 0, 0};//no ammo for any weapons by default
+        //ammo types correspond to weapon types according to integer values respectively
         static void Main(string[] args)
         {
+            equipped = 0;
             ShowHud();
+            
+            
         
         }
         static void ShowHud()
         {
-            Console.WriteLine("Weapon: ");
+            Console.WriteLine("Weapon: " + weapons[equipped]);
+            Console.ReadKey(true);
+        }
+        static void Fire()
+        {
+          
+        }
+        static void Reload()
+        {
+
         }
     }
 }
